3fe1a7b0ff8ab9b400fea5b75e5d5a30b3eff949
easylogic <cyberuls@gmail.com>
2023-07-13 19:44:28 +0900

    update storybook version

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,16 +1,13 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-controls",
-    "storybook-dark-mode",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-controls", "storybook-dark-mode", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {}
   },
+  docs: {
+    autodocs: true
+  }
 };
 export default config;
