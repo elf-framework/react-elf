931ebbe00a57edb28ae91780df9310a404715ddf
easylogic <cyberuls@gmail.com>
2023-07-13 16:44:20 +0900

    add @storybook/cli

--- apps/docs/src/components/actions/Menu.stories.tsx
+++ apps/docs/src/components/actions/Menu.stories.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import { Meta, Story } from "@storybook/react";
 import { Menu } from "@react-elf/menu";
 import "@elf-framework/design-system/style.css";
-import { FaBeer } from 'react-icons/fa';
-
 
 export default {
   component: Menu,
@@ -16,7 +14,7 @@ export default {
       control: {
         type: "select",
         options: ["menu", "dropdown", "contextmenu"],
-      }
+      },
     },
     hover: {
       name: "hover",
@@ -24,7 +22,7 @@ export default {
       type: { name: "boolean", required: false },
       control: {
         type: "boolean",
-      }
+      },
     },
     compact: {
       name: "compact",
@@ -32,57 +30,63 @@ export default {
       type: { name: "boolean", required: false },
       control: {
         type: "boolean",
-
-      }
-    }
+      },
+    },
   },
   args: {
     type: "menu",
     hover: false,
   },
   parameters: {
-    componentSubtitle: "Buttons should always have a label, unless they are only using an icon that is universally understood and accessible. ",
+    componentSubtitle:
+      "Buttons should always have a label, unless they are only using an icon that is universally understood and accessible. ",
     docs: {
       description: {
         component: `
 They can have an optional icon, but it should not be used for decoration. Use an icon only when necessary and when it has a strong association with the label text.
 
 The label can be hidden to create an icon-only button. If the label is hidden, an icon is required, and the label will appear in a tooltip.
-        `
-      }
-    }
-  }
+        `,
+      },
+    },
+  },
 } as Meta;
 export const DefaultMenu: Story = (args) => {
   const { type, hover, compact } = args;
-  return <div style={{ display: "flex", gap: 10, flexDirection: "column" }}>
-    <div style={{ display: "flex", gap: 10, padding: 100 }}>
-      <Menu
-        type={type}
-        compact={compact}
-
-        items={[{
-          type: "item",
-          title: "Item 1",
-        }, {
-          type: "divider",
-          dashed: true,
-        },
-        { type: "section", title: "Section title" },
-        {
-          type: "item",
-          title: "Item 2",
-        }, {
-          type: "item",
-          title: "Item 3",
-          hover: hover,
-          items: [{
-            type: "item",
-            title: "aaaa",
-          }]
-        }
-        ]} />
+  return (
+    <div style={{ display: "flex", gap: 10, flexDirection: "column" }}>
+      <div style={{ display: "flex", gap: 10, padding: 100 }}>
+        <Menu
+          type={type}
+          compact={compact}
+          items={[
+            {
+              type: "item",
+              title: "Item 1",
+            },
+            {
+              type: "divider",
+              dashed: true,
+            },
+            { type: "section", title: "Section title" },
+            {
+              type: "item",
+              title: "Item 2",
+            },
+            {
+              type: "item",
+              title: "Item 3",
+              hover: hover,
+              items: [
+                {
+                  type: "item",
+                  title: "aaaa",
+                },
+              ],
+            },
+          ]}
+        />
+      </div>
     </div>
-
-  </div>;
-}
+  );
+};
